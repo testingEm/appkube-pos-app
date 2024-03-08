fe5f4137ae1a9323426a58a603a2615f32022a8a
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-03-08 12:50:19 +0530

    created share screen and navigation

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -2,74 +2,72 @@ import React, { useState } from "react";
 import { View, Text, Pressable, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
+import styles from "./styles";
 
 const index = () => {
   // const [showShare, setshowShare] = useState(false);
   const navigation = useNavigation();
+  const [reloadScreen, setReloadScreen] = useState(false); // State to trigger screen reload
+
 
   // const handleOkClick = () => {
   //   setshowShare(true);
   // };
-  const goToOrders = () => {
-    navigation.navigate("Orders");
-  };
+  // const goToOrders = () => {
+  //   navigation.navigate("Orders");
+  // };
   const goToHome = () => {
     navigation.navigate("Home");
+     setReloadScreen(true);
   };
   const goToCustomers = () => {
     navigation.navigate("Customers");
+     setReloadScreen(true);
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View
-      //   style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5 }}
-      >
-        <Text style={{ color: "blue" }}>Paid sucessfully</Text>
-      </View>
-      <Pressable
-        style={{ backgroundColor: "blue", padding: 10, borderRadius: 5 }}
-        onPress={goToHome}
-      >
-        <Text style={{ color: "white", textAlign: "center", fontSize: 16 }}>
-          Go to Home
-        </Text>
-      </Pressable>
-      <Pressable
-        style={{ backgroundColor: "blue", padding: 10, borderRadius: 5 }}
-        onPress={goToOrders}
-      >
-        <Text style={{ color: "white", textAlign: "center", fontSize: 16 }}>
-          Go to Orders
-        </Text>
-      </Pressable>
-      <View
-        style={{
-          position: "absolute",
-          bottom: 20,
-          left: 30,
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 40,
-        }}
-      >
-        <Pressable style={{ fontSize: 16, fontWeight: 600 }} onPress={goToCustomers}>
-          {" "}
-          <Entypo name="share" size={20} color="black" />
-          Share
-        </Pressable>
+    // <View style={[styles.wrapper]}>
+      <View style={[styles.container]}>
+     
+     <Text style={[styles.boldText,styles.dark,styles.shadow]}>Paid sucessfully</Text>
+   {/*
+   <Pressable
+     style={{ backgroundColor: "blue", padding: 10, borderRadius: 5 }}
+     onPress={goToOrders}
+   >
+     <Text style={{ color: "white", textAlign: "center", fontSize: 16 }}>
+       Go to Orders
+     </Text>
+   </Pressable> */}
+   <View
+     style={[styles.btns]}
+   >
+     
+     <Pressable
+     style={[styles.bgLight,styles.btn]}
+     onPress={goToHome}
+   >
+       <Text style={[styles.dark,styles.font]}>Ok</Text>
+    
+   </Pressable>
+   <Pressable style={[styles.btn,styles.bgDark,]} onPress={goToCustomers}>
+      
+       <Entypo name="share" size={20} color="#d8f3dc" />
+       <Text style={[styles.light,styles.font]}>Share</Text>
+     </Pressable>
 
-        <TextInput
-          style={{
-            border: ".5px solid gray",
-            padding: 6,
+     {/* <TextInput
+       style={{
+         border: ".5px solid gray",
+         padding: 6,
 
-            flexDirection: "column",
-          }}
-          placeholder="please Enter your phone number"
-        ></TextInput>
-      </View>
-    </View>
+         flexDirection: "column",
+       }}
+       placeholder="please Enter your phone number"
+     ></TextInput> */}
+   </View>
+ </View>
+   
   );
 };
 
