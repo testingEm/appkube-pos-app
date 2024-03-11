5b4336c3b43ca00bd423d6917e4a722266b98192
Pimplay-Aakash <pimplayaakash@gmail.com>
2024-03-11 15:57:00 +0530

    made the changes in ui

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,8 +16,8 @@
 //   },
 // });
 
-import React, {useState, useEffect} from 'react';
-import {TouchableOpacity, Text, View, Pressable} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Text, View, Pressable } from 'react-native';
 import styles from './styles';
 // import { fetchCategories } from "../../Api/FetchProducts";
 // import {
@@ -26,63 +26,75 @@ import styles from './styles';
 // //   Fontisto,
 // } from "@expo/vector-icons";
 
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native';
 
-// import { addOrders} from "../../redux/slice/customerSlice";
+import { addOrders} from "../../redux/slice/customerSlice";
 
-import {fetchCategories} from '../../Api/FetchProducts';
+import { fetchCategories } from '../../Api/FetchProducts';
 
-import {AddAllProducts} from '../../redux/slice/getAllProductSlice';
-import {useDispatch} from 'react-redux';
-// import { fetchingOrders } from "../../api/fetchOrders";
+import { AddAllProducts } from '../../redux/slice/getAllProductSlice';
+import { useDispatch } from 'react-redux';
+import { fetchingOrders } from '../../Api/fetchOrders';
+
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
 
-  const [allProducts, setAllProducts] = useState([]);
+  const [, setAllProducts] = useState([]);
+  // const getProduct = async () => {
+  //   try {
+  //     const AllProducts = await fetchCategories();
+
+  //     setAllProducts(AllProducts.data.listProducts.items);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
   const getProduct = async () => {
     try {
       const AllProducts = await fetchCategories();
-
       setAllProducts(AllProducts.data.listProducts.items);
+      dispatch(AddAllProducts(AllProducts.data.listProducts.items));
     } catch (error) {
       console.error(error);
     }
   };
 
-  //   const fetchOrders = async () => {
-  //     try {
 
-  //       const response = await fetchingOrders();
-  //       const data = response.data.listOrders.items;
-  //       console.log("orders data", data);
-  //       data.map((value) => {
-  //         dispatch(addOrders(value));
+  const fetchOrders = async () => {
+    try {
 
-  //       });
+      const response = await fetchingOrders();
+      const data = response.data.listOrders.items;
+      console.log("orders data", data);
+      data.map((value) => {
+        dispatch(addOrders(value));
 
-  //     } catch (error) {
-  //       console.log("orders error", error);
+      });
 
-  //     }
-  //   };
+    } catch (error) {
+      console.log("orders error", error);
+
+    }
+  };
 
   useEffect(() => {
     getProduct();
-    // fetchOrders();
+    fetchOrders();
   }, []);
 
-  //   console.log("this is the allProducts for the redux",allProducts);
-  dispatch(AddAllProducts(allProducts));
+  // console.log("this is the allProducts for the redux",allProducts);
+  // dispatch(AddAllProducts(allProducts));
 
-    const navigation = useNavigation();
 
-    const handleGoToCheckout = () => {
-      navigation.navigate('Checkout');
-    };
-    const handleGoToAdduser = () => {
-      navigation.navigate('Adduser');
-    };
+  const handleGoToCheckout = () => {
+    navigation.navigate('Checkout');
+  };
+  const handleGoToAdduser = () => {
+    navigation.navigate('Adduser');
+  };
 
   return (
     <View style={[styles.container]}>
@@ -92,7 +104,7 @@ const Home = () => {
 
       <Pressable
         style={[styles.box, styles.bgDark]}
-          onPress={handleGoToAdduser}
+        onPress={handleGoToAdduser}
       >
         {/* <FontAwesome5 name="user-alt" size={18} color="#d8f3dc" /> */}
         <TouchableOpacity>
@@ -102,21 +114,21 @@ const Home = () => {
 
       <View style={[styles.box, styles.bgDark]}>
         {/* <AntDesign name="plus" size={18} color="#d8f3dc" /> */}
-        <Text style={[{textAlign: 'center'}, styles.light]}>
+        <Text style={[{ textAlign: 'center' }, styles.light]}>
           Add custom sale
         </Text>
       </View>
 
       <View style={[styles.box, styles.bgLight]}>
         {/* <FontAwesome5 name="percent" size={18} color="#31572c" /> */}
-        <Text style={[{textAlign: 'center'}, styles.dark]}>Apply discount</Text>
+        <Text style={[{ textAlign: 'center' }, styles.dark]}>Apply discount</Text>
       </View>
       <View style={[styles.box, styles.bgLight, ,]}>
         {/* <Fontisto name="shopping-basket" size={18} color="#31572c" /> */}
         <Text style={[styles.dark]}>Ship to customer</Text>
       </View>
       <TouchableOpacity
-        style={[styles.box, styles.active, {alignSelf: 'flex-start'}]}>
+        style={[styles.box, styles.active, { alignSelf: 'flex-start' }]}>
         {/* <AntDesign name="plus" size={20} color="#31572c" /> */}
         <Text style={[styles.dark]}>Add tile</Text>
       </TouchableOpacity>
